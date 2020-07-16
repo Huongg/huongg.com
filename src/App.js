b4a5ff0009eca7cf9f1ffc64258f2e953630a548
Huong Nguyen <huongg1409@gmail.com>
2020-07-16 16:52:16 +0100

    sticky navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.scss";
 
 import { NavButton } from "./components/nav-button/nav-button";
 
 function App() {
+  const [isSticky, setSticky] = useState(false);
+  const ref = useRef(null);
+  const handleScroll = () => {
+    if (ref.current) {
+      setSticky(ref.current.getBoundingClientRect().top <= 0);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", () => handleScroll);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="navigation">
+      <header className={`navigation${isSticky ? " sticky" : ""}`} ref={ref}>
         <NavButton>Home</NavButton>
         <NavButton>About</NavButton>
         <NavButton>Others</NavButton>
@@ -15,6 +30,7 @@ function App() {
         <NavButton>Travel</NavButton>
         <NavButton>Nerd</NavButton>
       </header>
+      <div className="main-page-image"></div>
     </div>
   );
 }
