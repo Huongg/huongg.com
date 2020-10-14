303dd11a520d2dc1ccce3ad86c34a323913fd18b
Huong Nguyen <huongg1409@gmail.com>
2020-10-14 18:20:48 +0100

    remove react template

--- src/areas/about-me/about-me.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./about-me.scss";
-
-export const AboutMe = () => {
-  return <h1 className="page-title">ABOUT ME</h1>;
-};
--- src/areas/home/home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import "./home.scss";
-
-export const Home = () => {
-  return <div className="main-page-image"></div>;
-};
