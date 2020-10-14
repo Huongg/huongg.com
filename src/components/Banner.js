7d1c89892e61ba02dd6227993fd0c1a1a05bc61a
Huong Nguyen <huongg1409@gmail.com>
2020-10-14 22:02:47 +0100

    new gatbsy template

---
+++ src/components/Banner.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Hi, my name is Huong Nguyen</h1>
+      </header>
+      <div className="content">
+        <p>
+          I'm passionate about building beautiful web-application
+          <br />
+          and capturing moments.
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              Find out more 
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+)
+
+export default Banner
